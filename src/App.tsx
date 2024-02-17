1af1f56cfcff02b5a4e43c1629de3e4be71e5987
iamtrxsh <aidanali65@gmail.com>
2024-02-17 01:20:50 -0500

    userAuthentication has been Finished appwrite Data&Queries

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
+import { Toaster } from "@/components/ui/toaster"
+
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 
@@ -21,6 +23,7 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+      <Toaster />
     </main>
   );
 };
