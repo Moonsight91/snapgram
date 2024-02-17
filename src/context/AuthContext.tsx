f2d3ba738748e6dd595a92054f3fa40f43c35fc7
iamtrxsh <aidanali65@gmail.com>
2024-02-17 01:51:41 -0500

    FInalized Commit for Auth/ Sign In Pages

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -60,8 +60,9 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
     useEffect(() => {
         if(
-            //localStorage.getItem('cookieFallback') === null
-            localStorage.getItem('cookieFallback') === '[]'
+            
+            localStorage.getItem('cookieFallback') === '[]'||
+            localStorage.getItem('cookieFallback') === null
         )navigate('/sign-in')
 
         checkAuthUser();
